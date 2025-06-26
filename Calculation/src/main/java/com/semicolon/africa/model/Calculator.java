f9cbed7bdbdc4d1806c3674bbdb60bc2bde82caa
GiftedGiftB <asekhamegift@gmail.com>
2025-06-26 21:16:39 +0100

    practicing

---
+++ Calculation/src/main/java/com/semicolon/africa/model/Calculator.java
@@ -0,0 +1,10 @@
+package main.java.com.semicolon.africa.model;//package main.java.com.semicolon.africa.model;
+public class Calculator {
+
+    public static int sumNumber(int number1, int number2){
+        int total = number1 + number2;
+        return total;
+
+    }
+
+}
