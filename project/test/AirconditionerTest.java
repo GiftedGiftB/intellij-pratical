89cee4d13aa57c0e52b4227cb4921e0177f81a1b
GiftedGiftB <asekhamegift@gmail.com>
2025-06-28 17:17:12 +0100

    Test for Airconditioner project

---
+++ project/test/AirconditionerTest.java
@@ -0,0 +1,10 @@
+import main.java.com.semicolon.africa.model.Airconditioner;
+import org.junit.jupiter.api.Test;
+
+public class AirconditionerTest {
+
+    @Test
+    public void airconditionIsOn_checkThatAirconditionIsOn(){
+        Airconditioner myAirconditioner = new Airconditioner();
+    }
+}
