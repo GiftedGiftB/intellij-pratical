c7205431eaaf0ec4c9d83703ce94bfb3c0f836a4
GiftedGiftB <asekhamegift@gmail.com>
2025-06-29 18:33:21 +0100

    Test for Airconditioner project

--- project/test/AccountTest.java
+++ project/test/AccountTest.java
@@ -7,43 +7,48 @@ public class AccountTest {
 
     @Test
     public void deposit(){
-        // Precondition
-        Account giftAccount = new Account(0);
+
+        Account giftAccount = new Account(0, "Gifted");
         assertEquals(0, giftAccount.getBalance());
 
-        // Action
         giftAccount.deposit(2_000);
         assertEquals(2_000, giftAccount.getBalance());
     }
     @Test
     public void withdraw(){
-        // Precaution
-      Account giftAccount = new Account(2_000);
+
+      Account giftAccount = new Account(2_000, "Gifted");
       assertEquals(2_000, giftAccount.getBalance());
 
-
-      //Action
-        giftAccount.withdraw(2_000);
+        giftAccount.withdraw(2_000, "Gifted");
         assertEquals(0, giftAccount.getBalance());
     }
     @Test
     public void withdrawAmountLargerThanBalance() {
-        // Precaution
-        Account giftAccount = new Account(5_000);
+
+        Account giftAccount = new Account(5_000, "Gifted");
         assertEquals(5_000, giftAccount.getBalance());
 
-        // Action
-        giftAccount.withdraw(6_000);
+        giftAccount.withdraw(6_000, "Gifted");
         assertEquals(5_000, giftAccount.getBalance());
     }
     @Test
     public void negativeDeposit_checkThatBalanceIsUnchanged(){
-        // Precaution
-        Account giftAccount = new Account(1_500);
+
+        Account giftAccount = new Account(1500, "Gifted");
         assertEquals(1500, giftAccount.getBalance());
 
-        // Action
         giftAccount.deposit(-2_000);
         assertEquals(1500, giftAccount.getBalance());
+    }
+    @Test
+    public void withdrawWithWrong_balanceRemainTheSame(){
+
+        Account giftAccount = new Account(1500, "Gifted");
+        assertEquals(1500, giftAccount.getBalance());
+
+        giftAccount.withdraw(1500, "ufyytfuy");
+        assertEquals(1500, giftAccount.getBalance());
+
     }
 }
