5c0be94d98ae5b806790076431b8e84ec0bf2c4a
GiftedGiftB <asekhamegift@gmail.com>
2025-06-29 18:33:59 +0100

    Airconditioner project

--- project/src/main/java/com/semicolon/africa/model/Airconditioner.java
+++ project/src/main/java/com/semicolon/africa/model/Airconditioner.java
@@ -1,6 +1,34 @@
 package main.java.com.semicolon.africa.model;
 
 public class Airconditioner {
-    private boolean myAircondioner;
+    private boolean itIsOn;
+    private boolean itIsOff;
+    private int temperature;
 
+    public Airconditioner(){
+        this.itIsOn = false;
+        this.itIsOff = false;
+    }
+    public void setMyAirconditionerToOn() {
+        itIsOn = true;
+    }
+    public boolean itIsOn() {
+        return itIsOn;
+    }
+
+    public void setMyAirconditionerToOff() {
+        itIsOff = true;
+    }
+    public boolean itIsOff() {
+        return itIsOff;
+    }
+    public void setMyAirconditionerTemperature(int control) {
+       if(control <= 30 && control >= 16){
+            temperature = control;
+            itIsOn = false;
+       }
+    }
+    public int getTemperature() {
+        return temperature;
+    }
 }
