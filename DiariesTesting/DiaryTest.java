5bd16103df08e1d5d73d47a912d9e94d4816ae07
GiftedGiftB <asekhamegift@gmail.com>
2025-07-22 23:40:13 +0100

    diary function

--- DiariesTesting/DiaryTest.java
+++ DiariesTesting/DiaryTest.java
@@ -1,26 +1,68 @@
 import DiariesPack.Diary;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DiaryTest {
+    Diary myDiary;
 
-    @Test
-    public void testThatDiaryCanBeCreated_DiaryCounterIsOne() {
-        Diary myDiary = new Diary("name" , "12345");
-        myDiary.DiaryCounterIsOne(1);
-        assertEquals(1, myDiary.DiaryCounterIsOne(1));
+    @BeforeEach
+    public void setUp() {
+        myDiary = new Diary("giftDiary","12345");
     }
     @Test
-    public void testThatDiaryIsUnlockedByDefault() {
-        Diary myDiary = new Diary("name" , "12345");
+    public void testThatDiary_IsUnlocked() {
         boolean isLocked = true;
         assertEquals(true, isLocked);
     }
     @Test
     public void testThatDiaryCanBeLocked() {
-        Diary myDiary = new Diary("name" , "12345");
+        myDiary.isLocked();
         boolean isLocked = true;
         assertEquals(true, isLocked);
     }
+    @Test
+    public void testThatEntriesIsNotEmpty() {
+        myDiary.createEntry("giftDiary" , "12345");
+        assertEquals(1, myDiary.getEntries().size());
+    }
+    @Test
+    public void testThatEntriesIsTwo(){
+        myDiary.createEntry("giftDiary" , "12345");
+        myDiary.createEntry("justineDiary" , "2468");
+        assertEquals(2, myDiary.getEntries().size());
+    }
+    @Test
+    public void testThatDiaryCanDelete(){
+        myDiary.unLock("12345");
+        assertEquals(true, myDiary.isLocked());
+        myDiary.createEntry("giftDiary" , "12345");
+        myDiary.createEntry("justineDiary" , "2468");
+        assertEquals(2, myDiary.getEntries().size());
+        myDiary.deleteEntry(2);
+        assertEquals(1, myDiary.getEntries().size());
+    }
+    @Test
+    public void testThatEntryCanBeFoundById(){
+        myDiary.createEntry("giftDiary" , "12345");
+        myDiary.createEntry("justineDiary" , "2468");
+        assertEquals(2, myDiary.getEntries().size());
+        myDiary.findEntryById(2);
+    }
+    @Test
+    public void testThatEntryCanBeUpdated(){
+        myDiary.createEntry("giftDiary" , "12345");
+        myDiary.createEntry("justineDiary" , "2468");
+        assertEquals(2, myDiary.getEntries().size());
+        myDiary.updateEntry(2);
+
+    }
+
+//    @Test
+//    public void testThatDiaryCanBeCreated_DiaryCounterIsOne() {
+//        Diary myDiary = new Diary("giftDiary" , "12345");
+//        myDiary.DiaryCounterIsOne(1);
+//        assertEquals(1, myDiary.DiaryCounterIsOne(1));
+//    }
 }
---
+++ project/src/DiariesPack/Entry.java
@@ -0,0 +1,31 @@
+package DiariesPack;
+
+public class Entry {
+    private int id;
+    private String title;
+    private String body;
+
+    public Entry(int id, String title, String body) {
+        this.id = id;
+        this.title = title;
+        this.body = body;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getBody() {
+        return body;
+    }
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
