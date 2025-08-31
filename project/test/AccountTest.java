2b3115cf40a8023f79edb01cca19a188ef38a06a
GiftedGiftB <asekhamegift@gmail.com>
2025-08-31 23:19:51 +0100

    task

--- project/test/AccountTest.java
+++ project/test/AccountTest.java
@@ -1,14 +1,18 @@
 import main.java.com.semicolon.africa.model.Account;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AccountTest {
-
+    Account giftAccount;
+    @BeforeEach
+    public void doThisFirst() {
+        Account giftAccount = new Account(0, "Gifted");
+    }
     @Test
     public void deposit(){
-
-        Account giftAccount = new Account(0, "Gifted");
+       // Account giftAccount = new Account(0, "Gifted");
         assertEquals(0, giftAccount.getBalance());
 
         giftAccount.deposit(2_000);
@@ -16,17 +20,15 @@ public class AccountTest {
     }
     @Test
     public void withdraw(){
-
-      Account giftAccount = new Account(2_000, "Gifted");
+        //Account giftAccount = new Account(2_000, "Gifted");
       assertEquals(2_000, giftAccount.getBalance());
 
         giftAccount.withdraw(2_000, "Gifted");
-        assertEquals(0, giftAccount.getBalance());
+        assertEquals(2_000, giftAccount.getBalance());
     }
     @Test
     public void withdrawAmountLargerThanBalance() {
-
-        Account giftAccount = new Account(5_000, "Gifted");
+        //Account giftAccount = new Account(5_000, "Gifted");
         assertEquals(5_000, giftAccount.getBalance());
 
         giftAccount.withdraw(6_000, "Gifted");
@@ -34,8 +36,7 @@ public class AccountTest {
     }
     @Test
     public void negativeDeposit_checkThatBalanceIsUnchanged(){
-
-        Account giftAccount = new Account(1500, "Gifted");
+        //Account giftAccount = new Account(1500, "Gifted");
         assertEquals(1500, giftAccount.getBalance());
 
         giftAccount.deposit(-2_000);
@@ -43,12 +44,13 @@ public class AccountTest {
     }
     @Test
     public void withdrawWithWrong_balanceRemainTheSame(){
-
-        Account giftAccount = new Account(1500, "Gifted");
+        //Account giftAccount = new Account(1500, "Gifted");
         assertEquals(1500, giftAccount.getBalance());
 
         giftAccount.withdraw(1500, "ufyytfuy");
         assertEquals(1500, giftAccount.getBalance());
+
+
 
     }
 }
