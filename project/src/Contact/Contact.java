80143699d134af441cd665a2d93bb5ea571d3905
GiftedGiftB <asekhamegift@gmail.com>
2025-08-31 23:25:51 +0100

    task

---
+++ project/src/Contact/Contact.java
@@ -0,0 +1,16 @@
+package Contact;
+
+public class Contact {
+    private String name;
+    private String address;
+    private String telephone;
+    private String email;
+
+
+    public Contact(String name, String address, String telephone, String email) {
+        this.name = name;
+        this.address = address;
+        this.telephone = telephone;
+        this.email = email;
+    }
+}
